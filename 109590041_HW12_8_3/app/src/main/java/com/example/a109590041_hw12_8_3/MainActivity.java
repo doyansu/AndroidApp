037f3129165ecffa03de9bcc9e740ba82b0f0ac5
yuanhao <t109590041@ntut.org.tw>
2022-06-01 23:35:16 +0800

    HW12

---
+++ 109590041_HW12_8_3/app/src/main/java/com/example/a109590041_hw12_8_3/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.a109590041_hw12_8_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.job.JobInfo;
+import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int JOB_ID = 0;
+    private JobScheduler mScheduler;
+
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
+    // Notification manager.
+    NotificationManager mNotifyManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createNotificationChannel();
+
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        ComponentName serviceName = new ComponentName(getPackageName(),
+                NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setRequiresDeviceIdle(true)
+                .setRequiresCharging(true)
+                .setPeriodic(TimeUnit.DAYS.toMillis(1));
+        JobInfo myJobInfo = builder.build();
+        mScheduler.schedule(myJobInfo);
+
+    }
+
+
+    public void Notification(View view)
+    {
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (this, 0, new Intent(this, MainActivity.class),
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder
+                (this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("Performing Work")
+                .setContentText("DownLoad in progress")
+                .setContentIntent(contentPendingIntent)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true);
+
+        mNotifyManager.notify(0, builder.build());
+    }
+
+    public void createNotificationChannel() {
+
+        // Define notification manager object.
+        mNotifyManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+
+            // Create the NotificationChannel with all the parameters.
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Job Service notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notifications from Job Service");
+
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+
+
+}
